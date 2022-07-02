c424b2155e0dabf5a4626a5f59ae13fab703612b
RomainC75 <rom.chenard@gmail.com>
2022-07-02 19:08:39 +0200

    reviews filter + seed pass encryption

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -5,20 +5,82 @@ const authentication = require('../middleware/authentication.mid')
 const Product = require('../models/Product.model')
 //query - filter : newest
 
-//time : newest, oldest //depends on the nFirst
-//quality : best, worst, x stars
-//nfirst: number
+// NO time : newest, oldest //depends on the nFirst
+//1 -- rate : number // min ou max !!
+//--->quality : best, worse
+// last  :nfirst: number
 
 router.get('/:productId', authentication, async (req,res,next)=>{
     try {
         const {productId} = req.params
-        const reviewsFound = await Review.find({userId:req.user._id})
-        if(reviewsFound.length===0){
-            res.status(404).json(reviewsFound)
-            return
+        //test there is a product with the productId
+        const product = Product.findById(productId)
+        if(product===null){
+            res.status(400).json({message: 'product not found !'})
+            return 
         }
-        const filterdList = reviewFound.filtered()
+
         console.log(req.query)
+        let reviewsFound = []
+        //if rate is a number
+        if('rate' in req.query && !isNaN(Number(req.query.rate))){
+            reviewsFound = await Review.find({$and : [ {productId:productId}  ,{ rate:req.query.rate }]}) 
+        //if rate is 'gte' or 'lte' and rateTreshold is a number
+        }else if('rate' in req.query && req.query.rate==='gte' && 'rateTreshold' in req.query && !isNaN(Number(req.query.rateTreshold))){
+            reviewsFound = await Review.find({$and : [ {productId:productId}  ,{ rate:{ $gte:req.query.rateTreshold } }]}) 
+        }else if('rate' in req.query && req.query.rate==='lte' && 'rateTreshold' in req.query && !isNaN(Number(req.query.rateTreshold))){
+            reviewsFound = await Review.find({$and : [ {productId:productId}  ,{ rate:req.query.rate }]}) 
+        }else{
+            reviewsFound = await Review.find({productId:productId}) 
+        }
+        //quality : 'best' of 'worse'
+        
+        if('quality' in req.query && req.query.quality==='worse'){
+            reviewsFound.sort((a,b)=>a.rate-b.rate)
+        }else {
+            reviewsFound.sort((a,b)=>b.rate-a.rate)
+        }
+
+        //pagination - page=2
+        const pagesInfo = {
+            page:0,
+            numberByPage:0,
+            totalPageNumber:0
+        }
+
+        if('numberByPage' in req.query && !isNaN(req.query.numberByPage)){
+            // numberByPage=2 is valid
+            if('page' in req.query && !isNaN(req.query.page)){
+                if(req.query.page*(req.query.numberByPage-1)>reviewsFound.length || req.query.page*req.query.numberByPage<=0){
+                    res.status(400).json({message:'wrong page/numberOfPage'})
+                    return
+                }
+                pagesInfo.page=parseInt(req.query.page),
+                pagesInfo.numberByPage=parseInt(req.query.numberByPage),
+                pagesInfo.totalPageNumber=Math.ceil(reviewsFound.length/req.query.numberByPage)
+                reviewsFound = reviewsFound.filter((review, i, arr)=>i>=((req.query.page-1)*req.query.numberByPage) && i<req.query.page*req.query.numberByPage )
+            }
+            //numberByPage=lkjdsf  is invalid
+            //default : page 1 and 3 reviewsByPage
+            else{
+                console.log('(pagelà')
+                pagesInfo.page=1,
+                pagesInfo.numberByPage=req.query.numberByPage
+                pagesInfo.totalPageNumber=Math.ceil(reviewsFound.length/req.query.numberByPage)-1
+                reviewsFound = reviewsFound.filter((review,i,arr)=>i<req.query.numberByPage)
+            }
+        }else{
+            //no numberByPage
+            pagesInfo.page=1,
+            pagesInfo.numberByPage=3
+            pagesInfo.totalPageNumber=Math.ceil(reviewsFound.length/3)-1
+            reviewsFound = reviewsFound.filter((review,i,arr)=>i<3)
+        }
+
+        console.log(pagesInfo)
+        //insert page AND numberByPage in the object
+        //const reviewsFound = await Review.find({productId:productId})
+        res.status(200).json({products:reviewsFound,pagesInfo})
 
     } catch (error) {
         next(error)
--- seed/seed.js
+++ seed/seed.js
@@ -1,11 +1,15 @@
 //require('dotenv').config('../.env')
 require('../db/index')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const {LoremIpsum} =  require ("lorem-ipsum");
+
 const Product = require('../models/Product.model')
 const Guitar = require('../models/Guitar.model')
 const Woodwind = require('../models/Woodwind.model')
 const String = require('../models/String.model')
 const User = require('../models/User.model')
+const Review = require('../models/Review.model')
 
 const bassList = require('./dataBass.json')
 const guitarList = require('./dataGuitar.json')
@@ -13,27 +17,69 @@ const stringList = require('./dataString.json')
 const woodList = require('./dataWoodwind.json')
 const usersList = require('./dataUser.json')
 
-const getRandomizedUser = require('./tools/randomUsers')
-
-
-//console.log(bassList)
+const reviewsCreation = async (guitarName) =>{
+    //create reviews about "guitarName"
+    const [guitarFound] = await Product.find({name:guitarName})
+    console.log('---->',guitarName,guitarFound)
+    //console.log(guitarFound)
+    console.log('LENGTH : ',usersList.length)
+    await Promise.all(usersList.map( async (user)=>{
+        const lorem = new LoremIpsum({
+            sentencesPerParagraph: {
+              max: Math.floor(Math.random()*8)+2,
+              min: 1
+            },
+            wordsPerSentence: {
+              max: Math.floor(Math.random()*16)+5,
+              min: 4
+            }
+          });
+        const [foundUser] = await User.find({email:user.email})
+        const paragraphes = lorem.generateParagraphs(Math.floor(Math.random()*5))
+        const ans = await Review.create({
+            userId:foundUser._id,
+            productId:guitarFound._id,
+            rate:Math.floor(Math.random()*5),
+            comment:paragraphes==="" ? "abac" : paragraphes
+        })
+    }))
+}
 
 const seed = async () =>{
     try{
-        //console.log(data)
         await Product.deleteMany()
         await Guitar.deleteMany()
         await Woodwind.deleteMany()
         await String.deleteMany()
-        
+        await User.deleteMany({"email":{$ne:"sponge.b0b@gmail.com"}})
+        await Review.deleteMany()
+
         const guitarAns = await Guitar.insertMany(guitarList)
         const bassAns = await Guitar.insertMany(bassList)
         const stringAns = await String.insertMany(stringList)
         const woodAns = await Woodwind.insertMany(woodList)
-        const userAns = await User.insertMany(usersList)
+
+        //const userAns = await User.insertMany(usersList)
+        await Promise.all( usersList.map(async(user)=>{
+            const salt = await bcrypt.genSalt(10)
+            const hash = await bcrypt.hash(user.password,salt)
+            await User.create({
+                email:user.email,
+                password:hash
+            })
+        }))
+        
+        //see tools/randomUsers.js to create a new JSON file with more or less randomUsers
         const answer = await Product.find({__t:"GuitarSchema"})
         
-            
+        await Promise.all( guitarList.map(async(guitar,i)=>{
+            //console.log('---->','i:',i,'++++',guitar)
+            await reviewsCreation(guitar.name)
+        }))
+        
+
+        await reviewsCreation()
+        
         mongoose.connection.close()
     }catch(e){
         console.log('EROR : ',e)
